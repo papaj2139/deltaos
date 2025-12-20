84d88b0756a001017d81e08b49dd26cb6c6aee5c
gingrspacecadet <jackrawlings2011@gmail.com>
2025-12-20 23:32:37 +0000

    shell stub

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -5,6 +5,7 @@
 #include <drivers/console.h>
 #include <drivers/keyboard.h>
 #include <kernel/device.h>
+#include <lib/string.h>
 
 void kernel_main(void) {
     serial_write("kernel_main started\n");
@@ -34,9 +35,42 @@ void kernel_main(void) {
     }
     
     //main kernel loop
-    while (1) {
-        char c;
-        if (get_key(&c)) con_putc(c);
-        arch_halt();
+    for (;;) {
+        con_print("[]$ ");
+
+        char buffer[128] = {0};
+        uint8 i = 0;
+        while (1) {
+            // optimisation: waits for keyboard interrupt before re-looping
+            keyboard_wait();
+
+            char c;
+            if (!get_key(&c)) continue;
+            if (c == '\b') {
+                if (i > 0) {
+                    i--;
+                    con_putc('\b');
+                }
+            } else {
+                if (i >= sizeof(buffer) - 1) {
+                    buffer[i] = 0;
+                    break;
+                }
+
+                con_putc(c);
+                buffer[i++] = c;
+                if (c == '\n') {
+                    buffer[i - 1] = '\0';
+                    break;
+                }
+            }
+        }
+
+        if (strcmp(strtok(buffer, " "), "help") == 0) {
+            con_print("HELP MEEEE\n");
+        } else if (buffer[0] != '\0') {
+            con_print(buffer);
+            con_print(": command not found\n");
+        }
     }
 }
