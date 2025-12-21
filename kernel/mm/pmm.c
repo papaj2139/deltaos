51dbba29f12ccb3d1b347e8a7ff6ede7a5f29acd
gingrspacecadet <jackrawlings2011@gmail.com>
2025-12-21 14:42:07 +0000

    fixed null deref

--- kernel/mm/pmm.c
+++ kernel/mm/pmm.c
@@ -7,15 +7,15 @@ void pmm_init(void) {
     set_outmode(SERIAL);
 
     uint64 total_usable = 0;
-    struct db_mmap_entry *greatest = {0};
+    struct db_mmap_entry greatest = {0};
     for (uint32 i = 0; i < mmap->entry_count; i++) {
         struct db_mmap_entry *current = mmap->entries + i;
         if (current->type == DB_MEM_USABLE) {
             printf("Usable memory: 0x%x - 0x%x (0x%x)\n", current->base, current->base + current->length, current->length);
             total_usable += current->length;
-            if (current->length > greatest->length) greatest = current;
+            if (current->length > greatest.length) greatest = *current;
         }
     }
     printf("Total available memory: 0x%x\n", total_usable);
-    printf("Greatest available segment: 0x%x - 0x%x (0x%x)\n", greatest->base, greatest->base + greatest->length, greatest->length);
+    printf("Greatest available segment: 0x%x - 0x%x (0x%x)\n", greatest.base, greatest.base + greatest.length, greatest.length);
 }
